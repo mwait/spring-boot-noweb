d51cbc34181ca2d2ffe3238d6b7b59e62f5da129
Mark Wait <m.wait@gmail.com>
2016-03-10 18:47:44 +0100

    ComponentScan add

--- src/main/java/application/SpringBootNowebApplication.java
+++ src/main/java/application/SpringBootNowebApplication.java
@@ -1,12 +1,14 @@
-package com.wait;
+package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.wait.controllers.GreetingController;
 
 @SpringBootApplication
+@ComponentScan("com.wait")
 public class SpringBootNowebApplication {
 
 	public static void main(String[] args) {
