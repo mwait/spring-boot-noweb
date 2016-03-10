bd570a36d4e814eeeaac572a164f1fd8354ab653
Mark Wait <m.wait@gmail.com>
2016-03-10 21:14:43 +0100

    Spring xml

--- src/main/java/application/SpringBootNowebApplication.java
+++ src/main/java/application/SpringBootNowebApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 import com.wait.controllers.GreetingController;
 
 @SpringBootApplication
-@ComponentScan({"com.wait", "config"})
+//@ComponentScan({"com.wait", "config"})
+@ImportResource("classpath:/spring/spring-config.xml")
 public class SpringBootNowebApplication {
 
 	public static void main(String[] args) {
